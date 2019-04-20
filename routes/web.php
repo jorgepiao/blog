5ac528f3ac8cc3e91fc9ceda726ea24f0c333108
jorgepiao <piaosin11@gmail.com>
2019-04-19 23:12:28 -0500

    Se agrego la plantilla para la pagina de administracion (dashboard) y su correspondiente ruta

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome', compact('posts'));
 });
 
-Route::get('posts', function(){
-    return App\Post::all();
+// Route::get('posts', function(){
+//     return App\Post::all();
+// });
+
+Route::get('admin', function(){
+    return view('admin.dashboard');
 });
